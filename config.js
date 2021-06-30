9d578cab4cc78cf4bfd3288d45239f562ef720bb
DinhWuanBao <86726777+DinhWuanBao@users.noreply.github.com>
2021-06-30 20:38:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hai Yen  à!',
+    introTitle: 'Hai Yen  à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Nay là tròn 1 năm Wuan Pao Thich HaiYen 😙',
-    desc: 'Hai Yen Lam Ngiu Quan Bao Nhe  ',
-    btnYes: 'Đồng Ý  <33',
+    desc: 'Hai Yen Lam Ngiu Quan Bao Nhe',
+    btnYes: 'Đồng Ý <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đưa bánh tráng  nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đưa bánh tráng nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
